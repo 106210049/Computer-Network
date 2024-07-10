c61d5f0cd258f9b99ef1b1e659b1b883e77182c3
Hoàng Bảo Long <105693455+106210049@users.noreply.github.com>
2024-07-10 23:14:05 +0700

    Update and rename python_socket_server.py to python_socket_server_TCP.py

--- python socket/python_socket_server_TCP.py
+++ python socket/python_socket_server_TCP.py
@@ -8,7 +8,11 @@ ADDR = (SERVER, PORT)
 FORMAT = 'utf-8'
 DISCONNECT_MESSAGE = "!DISCONNECT"
 
+# Create a server sie socket using IPv4 (AF_INET) and TCP protocol (SOCK_STREAM)
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# print(socket.gethostname()) # Get host name (device)
+# print(socket.gethostbyname(socket.gethostname())) # Get ID address of host
+# bind our new socket to a tuple (IP Address, Port Add)
 server.bind(ADDR)
 
 def handle_client(conn, addr):
@@ -22,22 +26,23 @@ def handle_client(conn, addr):
             msg = conn.recv(msg_length).decode(FORMAT)
             if msg == DISCONNECT_MESSAGE:
                 connected = False
-
-            print(f"[{addr}] {msg}")
+            print(f"[{addr}] Message recieved: {msg}")
             conn.send("Msg received".encode(FORMAT))
-        
+
     conn.close()
         
 
 def start():
+    # put socket into listening mode to listen for any possible connections
     server.listen()
     print(f"[LISTENING] Server is listening on {SERVER}")
     while True:
         conn, addr = server.accept()
+        # conn.send("You are connected".encode(FORMAT))
         thread = threading.Thread(target=handle_client, args=(conn, addr))
         thread.start()
+        
         print(f"[ACTIVE CONNECTIONS] {threading.activeCount() - 1}")
-
-
+        
 print("[STARTING] server is starting...")
 start()
